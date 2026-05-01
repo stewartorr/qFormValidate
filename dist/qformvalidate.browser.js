0d2d3947c231892f2d9f0b8b1eb186a142a98c60
Stewart Orr <stewart.orr@gmail.com>
2026-05-01 11:59:10 +0100

    build: update to work with cdn

--- dist/qformvalidate.browser.js
+++ dist/qformvalidate.browser.js
@@ -1,34 +1,5 @@
 "use strict";
-var qFormValidate = (() => {
-  var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
-  var __export = (target, all) => {
-    for (var name in all)
-      __defProp(target, name, { get: all[name], enumerable: true });
-  };
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-    }
-    return to;
-  };
-  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-  // src/index.ts
-  var index_exports = {};
-  __export(index_exports, {
-    isValidEmail: () => isValidEmail,
-    isValidTel: () => isValidTel,
-    isValidURL: () => isValidURL,
-    patterns: () => patterns,
-    qFormValidate: () => qFormValidate,
-    trim: () => trim
-  });
-
+(() => {
   // src/utils.ts
   var patterns = {
     dateOnly: /^[0-9/.-]+$/,
@@ -307,6 +278,8 @@ var qFormValidate = (() => {
       return !hasError;
     }
   }
-  return __toCommonJS(index_exports);
+
+  // src/browser.ts
+  globalThis.qFormValidate = qFormValidate;
 })();
 //# sourceMappingURL=qformvalidate.browser.js.map
---
+++ src/browser.ts
@@ -0,0 +1,3 @@
+import { qFormValidate } from './qformvalidate.js';
+
+(globalThis as any).qFormValidate = qFormValidate;
